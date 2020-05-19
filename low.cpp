72e911f7503c2d5a020f9680f66c9d3b4f678893
4bselyam <45922762+4bselyam@users.noreply.github.com>
2020-05-19 23:25:49 +0300

    Update low.cpp

--- low.cpp
+++ low.cpp
@@ -1,69 +1,76 @@
 #include "Stdafx.h"
 using namespace std;
+#define count_monitor 4
 /*
-Создать бинарный файл с информацией о работниках телестудии: 
-    - фамилия;
-    - должность;
-    - образование;
-    - дата приема на работу;
-    - пол. 
-Вывести сведения о работниках телестудии, которые работают на должности инженеров, но не имеющие 
-высшего образования
+Ñîçäàòü áèíàðíûé ôàéë ñ èíôîðìàöèåé î ïðåäëîæåííûõ ê ðåàëèçàöèè ìîíèòîðàõ:
+	- íàçâàíèå ôèðìû;
+	- ðàçìåð ïî äèàãîíàëè;
+	- ñòîèìîñòü. 
+Îïðåäåëèòü ñðåäíþþ öåíó ìîíèòîðîâ, ðàçìåðîì íå ìåíåå 19 äþéìîâ è ðàñïå÷àòàòü ñâåäåíèÿ î íèõ
 */
-template <typename T>
-string toString(T val)
+#ifndef _monitor
+#define _monitor
+struct Monitor
 {
-    std::ostringstream oss;
-    oss << val;
-    return oss.str();
-}
-
-void low()
+	string		   company_name;
+	unsigned short diagonal_size;
+	unsigned int   price;    
+} monitor[count_monitor];
+#endif // !_monitor
+void ShowInfo(Monitor* mas_mon)
 {
-    system("cls");
-    printf("\nLow task level:\n");
-    int count_workers = 10;
-    WorkerTV* workers = new WorkerTV[count_workers];
-    for (size_t i = 0; i < count_workers; i++)
-    {
-        workers[i].surname   = "Worker" + toString(i + 1);
-        workers[i].position  = ((rand() % 2) == 1) ? "enginer" : "anyworker";
-        workers[i].education =  rand() % 2;
-       // workers[i].employment_date  = ;
-        workers[i].gender    = ((rand() % 2) == 1) ? "man" : "woman";
-    }
+	cout << "\nÑâåäåíèÿ, î ìîíèòîðàõ, õðàíÿùèåñÿ â ôàéëå <<monitor.dat>>\n";
+	for (size_t i = 0; i < count_monitor; i++)
+	{
+		cout <<"\t"<<i+1<<"."<< mas_mon[i].company_name << " " << mas_mon[i].diagonal_size << " " << mas_mon[i].price << endl;
 
-    string filename = "WorkerTVInfo";
-    ofstream f_out; f_out.open(filename, ios::binary);
-    int buffersize = sizeof(WorkerTV) * count_workers;
-    f_out.write((char*)workers, buffersize); f_out.close();
-
-    WorkerTV* buf = new WorkerTV[count_workers];
-    ifstream f_in; f_in.open(filename, ios::binary);
-    f_in.read((char*)buf, buffersize); f_in.close();
-
-    cout << "\nИнформация прочитанная с файла <<WorkerTVInfo.bin>>: \n\t";
-    for (size_t i = 0; i < count_workers; i++)
-    {
-        cout << workers[i].surname<< " ";
-        cout << workers[i].position << " ";
-        cout << workers[i].education << " ";
-        cout << workers[i].gender <<"\n\t";
-    }
-
-    cout << "\nCведения о работниках телестудии, которые работают на должности инженеров, но не имеющие\n" 
-        "высшего образования прочитанные с файла <<WorkerTVInfo.bin>>: \n\t";
-    for (size_t i = 0; i < count_workers; i++)
-    {
-        if (workers[i].position== static_cast<string>("enginer") && workers[i].education) {
-            cout << workers[i].surname << " ";
-            cout << workers[i].position << " ";
-            cout << workers[i].education << " ";
-            cout << workers[i].gender << "\n\t";
-        }
-    }
-    cout << "\nНажми Enter для завершения...\n";
-    while (getchar() != '\n') { continue; }
+	}
 }
+void WorkFunc(Monitor* mas_mon)
+{
+	cout << "\nÑâåäåíèÿ î ìîíèòîðàõ ó êîòîðûõ ðàçìåð ïî äèàãîíàë íå ìåíåå 19 äþéìîâ, "
+		"è èõ ñðåäíÿÿ \nñòîèìîñòü\n";
+	double sum=0;
+	int count = 0;
+	bool flag = false;
+	for (size_t i = 0; i < count_monitor; i++)
+	{
+		if ( mas_mon[i].diagonal_size >= 19 )
+		{
+			flag = true;
+			cout << "\t" << i + 1 << "." << mas_mon[i].company_name << " " 
+				          << mas_mon[i].diagonal_size << " " << mas_mon[i].price << endl;
+			sum += mas_mon[i].price; count++;
+		}
+		
+	}
+	flag ? cout << "\tCðåäíÿ ñòîèìîñòü " << (double)(sum / count) << endl : 
+		             cout << "\tÒàêèõ ìîíèòîðîâ íåò !\n";
+}
+void Base()
+{
+	cout << "\nBase level\n";
+	FILE* f_out, * f_in;
+	char path[] = "monitor.dat";
+	f_out = fopen(path, "wb");
+	string mas_company_name[] = {"LG", "Sumsung", "Asus", "Acer"} ;
+	cout << "Äàííûå õðàíÿùèåñÿ â ôàéëå <<monitor.dat>>: \n";
+	for (size_t i = 0; i < count_monitor; i++)
+	{
+		cout<< ( monitor[i].company_name = mas_company_name[i]) << "\t";
+		cout<< ( monitor[i].diagonal_size = 10 + rand() % 20 ) <<"\t";
+		cout<< ( monitor[i].price = 1000 + rand() % 20000 ) <<endl;
+	}
 
+	Monitor* buffer = new Monitor[count_monitor];
+	//size_t size_mon = sizeof(Monitor);
+	fwrite(monitor, sizeof(Monitor), count_monitor, f_out);
+	fclose(f_out);
+	f_in = fopen(path, "rb");
+	fread(buffer, sizeof(Monitor), count_monitor, f_in);
+	ShowInfo(buffer);
+//	system("cls");
+	WorkFunc(buffer);
+	fclose(f_in);
 
+}
